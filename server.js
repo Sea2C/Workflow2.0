2e3a2bd4c8c1ac2efa9f47fba6ccabdb9138dfec
Colin Teal <the30yearswar@hotmail.com>
2013-05-06 23:36:55 -0700

    Scale Node process by number of processors

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
-var http = require('http'),
+var cpus,
+    workers,
+    listening,
+    http = require('http'),
     app = require('./lib'),
-    port = app.get('port');
+    port = app.get('port'),
+    cluster = require('cluster');
 
-http.createServer(app).listen(port, function () {
-    console.log('Express server listening on port %d', port);
-    console.log('Press Ctrl+C to exit...')
-});
+if (cluster.isMaster) {
+    workers = listening = cpus = require('os').cpus().length;
+
+    while(--workers >= 0) {
+        cluster.fork();
+    }
+
+    // listen for the sake of indicating when the server is ready
+    cluster.on('listening', function (worker, address) {
+        if (!(--listening)) {
+            console.log('%d Express server(s) listening on port %d', cpus, port);
+            console.log('Press Ctrl+C to exit...');
+        }
+    });
+
+    // restart a worker when it unexpectedly exits
+    cluster.on('exit', function (worker, code, signal) {
+        var process = worker.process;
+        console.log('Worker %s died (%s). restarting...', process.pid, process.exitCode);
+        cluster.fork();
+    });
+} else {
+    // start the server
+    http.createServer(app).listen(port);
+}
